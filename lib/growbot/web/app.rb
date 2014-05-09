14d857aced55bcda702dca486b3188e5eb337dfa
David Long <dave@davejlong.com>
2014-05-09 09:08:41 -0400

    Prepped code for use as a gem

--- lib/growbot/web/app.rb
+++ lib/growbot/web/app.rb
@@ -10,12 +10,11 @@ module Growbot
   module Web
     class App < Sinatra::Base
       configure do
-        set :root, Growbot::Web::Env.root
-
-        set :public_folder, File.join(Growbot::Web::Env.root, 'public')
+        set :root, File.expand_path(File.dirname(__FILE__))
+        set :public_folder, File.join(settings.root, 'public')
         set :haml, format: :html5, layout: :layout
-        set :scss, style: :expanded, views: 'views/sass'
-        set :coffee, views: 'views/coffee'
+        set :scss, style: :expanded, views: File.join(settings.root, 'views/sass')
+        set :coffee, views: File.join(settings.root, 'views/coffee')
       end
 
       get '/' do
